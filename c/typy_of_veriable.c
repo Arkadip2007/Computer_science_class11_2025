7145207f8ac9550b8dccfba2b397e7fc2981bef0
Arkadip2007 <arkadipmahapatra2007@gmail.com>
2025-06-21 16:52:18 +0530

    c

---
+++ c/typy_of_veriable.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a;//--"a"is the variable ,type -->integer
+    a=10;//the value of a is 10
+    printf("%d",a);// %d use to the value of . and 
+    return 0;
+}
