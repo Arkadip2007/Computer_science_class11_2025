1d9e00ebd54972d6b23f89423424519f331e6139
Arkadip2007 <arkadipmahapatra2007@gmail.com>
2025-11-29 01:36:12 +0530

    Update LED control sketch to support 13 LEDs and add new Python file

--- Microcontroller/Arduino/sketch_nov27a/sketch_nov27a.ino
+++ Microcontroller/Arduino/sketch_nov27a/sketch_nov27a.ino
@@ -1,5 +1,5 @@
-const int totalLEDs = 7;
-int ledPins[7] = { 7, 8, 9, 10, 11, 12, 13 };
+const int totalLEDs = 13;
+int ledPins[13] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0 };
 
 void setup() {
   for (int i = 0; i < totalLEDs; i++) {
