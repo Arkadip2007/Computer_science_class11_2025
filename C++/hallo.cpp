68c4cdc7df170e6a48bd5a433547fd90e3ea4b73
Arkadip2007 <arkadipmahapatra2007@gmail.com>
2025-12-19 22:57:42 +0530

    Add initial implementations of Hello World programs in C#, C++, and Java

---
+++ C++/hallo.cpp
@@ -0,0 +1,7 @@
+//hallo world in C++
+#include <iostream>
+int main() 
+{
+    std::cout << "hallo world" << std::endl;// std means standard output stream ,cout means character output stream, endl means end 
+    return 0;
+}
