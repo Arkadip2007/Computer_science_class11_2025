5ebb6d7427b6b8c8f161d43ee09574a47a2dfe81
Arkadip2007 <arkadipmahapatra2007@gmail.com>
2025-12-30 00:07:56 +0530

    Add name.bin file with author information

--- Microcontroller/Arduino/lcd_display_ch/lcd_display_ch.ino
+++ Microcontroller/Arduino/lcd_display_ch/lcd_display_ch.ino
@@ -1,20 +1,60 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Most common I2C address = 0x27
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// Custom characters
+byte heart[8] = {
+  B00000,
+  B01010,
+  B11111,
+  B11111,
+  B11111,
+  B01110,
+  B00100,
+  B00000
+};
+
+byte battery[8] = {
+  B01110,
+  B10101,
+  B10101,
+  B10101,
+  B10101,
+  B10101,
+  B11111,
+  B11111
+};
+
+byte wifi[8] =   {
+  B00000,
+  B00100,
+  B01010,
+  B10001,
+  B00100,
+  B11111,
+  B01110,
+  B00100
+};
+
 void setup() {
-  lcd.init();        // initialize LCD
-  lcd.backlight();   // turn on backlight
+  lcd.init();
+  lcd.backlight();
+
+  lcd.createChar(0, heart);
+  lcd.createChar(1, battery);
+  lcd.createChar(2, wifi);
 
   lcd.setCursor(0, 0);
-  lcd.print("Hello !");
+  lcd.print("Icons:");
 
   lcd.setCursor(0, 1);
-  lcd.print("its arkadip");
+  lcd.write(0); // heart
+  lcd.print(" ");
+  lcd.write(1); // battery
+  lcd.print(" ");
+  lcd.write(2); // wifi
 }
 
 void loop() {
-  // nothing to do
 }
