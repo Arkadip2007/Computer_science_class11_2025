8abdb2b4972a320a5a6c21d6a68c2f71c80e19e1
Arkadip2007 <arkadipmahapatra2007@gmail.com>
2026-01-01 09:31:59 +0530

    Add interactive number addition feature to laptop_test.c

--- C/laptop_test.c
+++ C/laptop_test.c
@@ -3,5 +3,12 @@ int main ()
 {
     printf("this code is writen to test my laptop setup for git and github. \n");
     printf("i (computer) see this massage.ok git and github setup full completed. \n");
+    int a,b;
+    printf("enter number \n");
+    scanf("%d",&a);
+    printf("enter anther number \n");
+    scanf("%d",&b);
+    int sum = a + b;    
+    printf("the sum of %d and %d is %d \n",a,b,sum);
     return 0;
 }
